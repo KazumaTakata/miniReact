4fef010113475bc500b43add72c63907b0d7f949
Kazuma Takata <neilbuzztakata@gmail.com>
2019-07-13 22:37:46 +0900

    change package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDom from './lib/reactdom'
 import Component from './test/refs/ref'
 
-let component = new Component()
+// let main = document.getElementById('main')
+// ReactDom.render(component, main)
 
-let main = document.getElementById('main')
-ReactDom.render(component, main)
+export { ReactDom, Component }
